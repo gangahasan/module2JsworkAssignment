1c30862c8813af3f4a8ec2382ffe5268aacb9e01
gangahasan <rgangabhavani2@gmail.com>
2024-12-09 11:14:32 +0530

    global and functional context

---
+++ questionNo19.js
@@ -0,0 +1,14 @@
+
+let age = 23;
+
+function displayAge(){
+     return age;
+}
+ 
+function changeAge(age){
+    return age;
+}
+ console.log(displayAge());
+ console.log(changeAge(34));
+ console.log(displayAge());
+ console.log(changeAge(25));
