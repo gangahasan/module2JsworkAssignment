5438ddd956c523ef6b4ae9c976c2b562e5d65848
gangahasan <rgangabhavani2@gmail.com>
2024-12-10 12:02:32 +0530

    2nd question added

--- questionNo20.js
+++ questionNo20.js
@@ -19,3 +19,29 @@ const counter = createCounter();
 console.log(counter.increment()); 
 console.log(counter.increment()); 
 console.log(counter.decrement()); 
+
+function createBankAccount(){
+  let balance = 0;
+  return {
+    deposit:function(depositamount){
+      balance += depositamount;
+      return `Deposited: ${balance}`;
+    },
+    withdraw: function(withdraw){
+      if(withdraw<=balance){
+        balance = balance-withdraw;
+        return `Withdrawn: ${withdraw}`;
+      }else{
+        return "Insufficient balance";
+      }
+    },
+    balance:function(){
+      return balance;
+    },
+  }
+}
+const account = createBankAccount();
+console.log(account.deposit(500)); // Output: Deposited: 500
+console.log(account.withdraw(200)); // Output: Withdrawn: 200
+console.log(account.withdraw(400)); // Output: Insufficient balance
+console.log(account.balance()); 
